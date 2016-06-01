40ccc0137ed7c617190957a28ddf0d95819f2a61
tanishkgupta <emailcaprio792@gmail.com>
2016-06-01 19:21:00 +0530

    coming soon

---
+++ assets/js/functions.js
@@ -0,0 +1,140 @@
+
+/* Background Images
+-------------------------------------------------------------------*/
+var  pageTopImage = jQuery('#page-top').data('background-image');
+var  aboutImage = jQuery('#about').data('background-image');
+var  subscribeImage = jQuery('#subscribe').data('background-image');
+
+
+if (pageTopImage) {  jQuery('#page-top').css({ 'background-image':'url(' + pageTopImage + ')' }); };
+if (aboutImage) {  jQuery('#about').css({ 'background-image':'url(' + aboutImage + ')' }); };
+if (subscribeImage) {  jQuery('#subscribe').css({ 'background-image':'url(' + subscribeImage + ')' }); };
+
+
+/* Background Images End
+-------------------------------------------------------------------*/
+
+
+
+/* Document Ready function
+-------------------------------------------------------------------*/
+jQuery(document).ready(function($) {
+	"use strict";
+
+
+    /* Window Height Resize
+    -------------------------------------------------------------------*/
+    var windowheight = jQuery(window).height();
+    if(windowheight > 650)
+    {
+         $('.pattern').removeClass('height-resize');
+    }
+    /* Window Height Resize End
+    -------------------------------------------------------------------*/
+
+
+    
+	/* Main Menu   
+	-------------------------------------------------------------------*/
+	$('#main-menu #headernavigation').onePageNav({
+		currentClass: 'active',
+		changeHash: false,
+		scrollSpeed: 750,
+		scrollThreshold: 0.5,
+		scrollOffset: 0,
+		filter: '',
+		easing: 'swing'
+	});  
+
+	/* Main Menu End  
+	-------------------------------------------------------------------*/
+
+
+
+
+	/* Time Countdown 
+	-------------------------------------------------------------------*/
+	$('#time_countdown').countDown({
+        
+        // targetDate: {
+        //     'day': 10,
+        //     'month': 6,
+        //     'year': 2016,
+        //     'hour': 7,
+        //     'min': 8,
+        //     'sec': 9
+        // },
+        // omitWeeks: true
+
+         targetOffset: {
+            'day':      10,
+            'month':    0,
+            'year':     0,
+            'hour':     7,
+            'min':      8,
+            'sec':      9
+		},
+		omitWeeks: true
+
+	    });
+
+
+
+	/* Next Section   
+	-------------------------------------------------------------------*/
+	$('.next-section .go-to-about').click(function() {
+    	$('html,body').animate({scrollTop:$('#about').offset().top}, 1000);
+  	});
+  	$('.next-section .go-to-subscribe').click(function() {
+    	$('html,body').animate({scrollTop:$('#subscribe').offset().top}, 1000);
+  	});
+
+  	$('.next-section .go-to-page-top').click(function() {
+    	$('html,body').animate({scrollTop:$('#page-top').offset().top}, 1000);
+  	});
+
+  	/* Next Section End
+	-------------------------------------------------------------------*/
+
+
+
+
+  /* Subscribe
+  -------------------------------------------------------------------*/
+    $(".news-letter").ajaxChimp({
+        callback: mailchimpResponse,
+        url: "" // Replace post url inside double quote "".  
+    });
+
+    function mailchimpResponse(resp) {
+         if(resp.result === 'success') {
+         
+            $('.alert-success').html(resp.msg).fadeIn().delay(3000).fadeOut();
+            
+        } else if(resp.result === 'error') {
+            $('.alert-warning').html(resp.msg).fadeIn().delay(3000).fadeOut();
+        }  
+    };
+
+
+
+
+	/* Subscribe End
+	-------------------------------------------------------------------*/
+
+});
+
+/* Document Ready function End
+-------------------------------------------------------------------*/
+
+
+/* Preloder 
+-------------------------------------------------------------------*/
+$(window).load(function () {    
+    "use strict";
+    $("#loader").fadeOut();
+    $("#preloader").delay(500).fadeOut("slow");
+});
+ /* Preloder End
+-------------------------------------------------------------------*/
+   
